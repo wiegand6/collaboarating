ec90792433b6d2a3e630f3d08b8168a8670e90b5
casher-ou <casher@oakland.edu>
2022-12-05 22:17:38 -0500

    database linking + addition of forumposts page

--- collab/forum.py
+++ collab/forum.py
@@ -9,11 +9,58 @@ bp = Blueprint('forum', __name__, url_prefix='/forum')
 
 @bp.route('/forumpage', methods=('GET', 'POST'))
 def forum():
+    if request.method == 'POST':
+        post_username = request.form['post_username']
+        post_title = request.form['post_title']
+        post_text = request.form['post_text']
+        db = get_db()
+        error = None
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO forumPost (post_username, post_title, post_text) VALUES (?, ?, ?)",
+                    (post_username, post_title, post_text),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"Post already exists."
+
+    if request.method == 'GET':
+        db = get_db()
+        db.execute('SELECT * FROM forumPost')
+
+
     # serve forumpage.html
     return render_template("forum/forumpage.html")
 
 
 @bp.route('/thread', methods=('GET', 'POST'))
 def thread():
-    # server threads
+    db = get_db()
+    if request.method =='POST':
+        title = request.form['title']
+        username = request.form['username']
+        comment = request.form['comment']
+        error = None
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO forumComment (title, username, comment) VALUES (?, ?, ?)",
+                    (title, username, comment),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"comment already exists."
+
+    # serve server thread template
     return render_template("forum/thread.html")
+
+
+@bp.route('/forumposts', methods=('GET', 'POST'))
+def forumposts():
+    db = get_db()
+    results = db.execute('SELECT * FROM forumPost')
+    return render_template("forum/forumposts.html", results=results)
+
--- collab/schema.sql
+++ collab/schema.sql
@@ -6,3 +6,19 @@ CREATE TABLE user (
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
+
+CREATE TABLE forumPost (
+    post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_username TEXT NOT NULL,
+    post_title TEXT  NULL,
+    post_text TEXT NULL,
+    post_time TEXT DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE forumComments (
+    post_id INTEGER PRIMARY KEY,
+    comment_id INTEGER AUTOINCREMENT,
+    comment_username TEXT NOT NULL,
+    comment_time TEXT DEFAULT CURRENT_TIMESTAMP,
+    comment_text VARCHAR MAX NULL
+)
